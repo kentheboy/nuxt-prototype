b706b8433a952b2ff22ae84927958222c62cb55b
kentheboy <foryoukentheboy1020@gmail.com>
2025-04-29 00:38:04 +0800

    initial commit (nuxt js project created)

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer /> -->
+    <NuxtPage />
+    <AppAlert>
+      This is an auto-imported component.
+    </AppAlert>
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup>
+const count = ref(1) // ref is auto-imported
+console.log('count', count.value);
+console.log("hello world");
+</script>

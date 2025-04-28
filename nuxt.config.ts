cac350100f69de547bd730874709938b80c4bee1
kentheboy <foryoukentheboy1020@gmail.com>
2025-04-29 01:21:56 +0800

    add Tailwind CSS integration and update configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/nuxt-config
-import { resolve } from 'path';
+import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite"; // https://tailwindcss.com/docs/installation/framework-guides/nuxt
 export default defineNuxtConfig({
-    alias: {
-        '@': resolve(__dirname, './src'),
-    },
-    css: ['@/assets/css/main.scss'],
-})
+  alias: {
+    "@": resolve(__dirname, "./src"),
+  },
+  css: ["@/assets/css/main.scss"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});

09bf3b41e9abc53645eff74c1456a288674e5617
kentheboy <foryoukentheboy1020@gmail.com>
2025-04-29 00:44:25 +0800

    remove unused components and scripts from app.vue

--- app.vue
+++ app.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
-    <!-- <NuxtRouteAnnouncer /> -->
-    <NuxtPage />
-    <AppAlert>
-      This is an auto-imported component.
-    </AppAlert>
-    <NuxtWelcome />
+    Hello Nuxt 3
   </div>
 </template>
-<script setup>
-const count = ref(1) // ref is auto-imported
-console.log('count', count.value);
-console.log("hello world");
-</script>
